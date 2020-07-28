4b45905d54ce7c28723ca709fabdf569e41daf69
GustavoCCarrillo <g2c100@gmail.com>
2020-07-28 15:44:57 -0700

    Add files via upload

---
+++ CalculateTuition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculateTuition
+{
+    class Program
+    {
+        public static string TuitionCalculator(int year)
+        {
+            double tuition = 6000;
+            string finalTuition;
+            for (int i = 1; i <= year; i++)
+            {
+                tuition += tuition * 0.2;
+            }
+            return finalTuition = $"For year {year} your tuition will be {tuition}";
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TuitionCalculator(3));
+        }
+    }
+}
