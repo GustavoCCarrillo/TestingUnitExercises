f6e32a7bfb64f2521891dd6e89ec3a35578e2425
GustavoCCarrillo <g2c100@gmail.com>
2020-07-28 15:39:35 -0700

    Add files via upload

---
+++ TimeFunctionTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TimeFunction;
+
+
+//namespace TimeFunctionTest
+//{
+//    /*[TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        /* public void TestMethod1()
+//        {
+//            Assert.AreEqual(15, TimeFunction.TimeFunctionClass.TimeBetween(2, 5, 1, 10));
+//        } */
+
+//        [TestMethod]
+//        /*public void TestMethod2()
+//        {
+//            Assert.AreEqual(15, TimeFunction.TimeFunctionClass.TimeBetween(1, 12, 7, 9));
+//            Assert.AreEqual(15, TimeFunction.TimeFunctionClass.TimeBetween(9, 3, 11, 2));  
+//            Assert.AreEqual(15, TimeFunction.TimeFunctionClass.TimeBetween(10, 5, 19, 5));
+//        } */
+//    }*/
+//}
