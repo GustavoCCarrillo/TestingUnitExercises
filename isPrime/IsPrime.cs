33d3080dfb9b8f7fa449b4290d59b2eac3ae5834
GustavoCCarrillo <g2c100@gmail.com>
2020-07-28 15:37:59 -0700

    Add files via upload

---
+++ isPrime/IsPrime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace isPrime
+{
+    public class IsPrime
+    {
+        public static bool IsPrimeNumber(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
