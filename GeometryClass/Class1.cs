aebc596aadd8486f80daeac622da6ab782827f12
GustavoCCarrillo <g2c100@gmail.com>
2020-07-28 15:32:47 -0700

    Add files via upload

---
+++ GeometryClass/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GeometryClass
+{
+    public class Triangle
+    {
+        public static double AreaOfTriangle(double height, double triangleBase) => (triangleBase * height) / 2.0;
+
+        public static double Hypoteneuse(double SideA, double SideB) => Math.Sqrt(SideA * SideA + SideB * SideB);
+    }
+}
