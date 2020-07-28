3fa2ae3e2890af93b6bda5daa666a6dfebe8ef57
GustavoCCarrillo <g2c100@gmail.com>
2020-07-28 15:36:06 -0700

    Add files via upload

---
+++ ReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class Program
+    {
+        public static string ReverseString(string theString)
+        {
+            string reversedString = "";
+
+            for (int i = 0; i < theString.Length; i++)
+            {
+                reversedString += theString[theString.Length - 1 - i];
+            }
+            return reversedString;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{ReverseString("Gustavo")}");
+        }
+    }
+}
